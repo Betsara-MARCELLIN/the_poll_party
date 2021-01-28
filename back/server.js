cd6d3ee353808505459df738452ffe2cd6c5b41d
Aldric-Ducreux <neogeekmo@live.fr>
2021-01-28 21:15:04 +0100

    add get / emit message

--- back/server.js
+++ back/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const http = require("http");
-const socketIo = require("socket.io");
 
 const port = process.env.PORT || 3000;
 const index = require("./routes/index");
@@ -10,19 +9,31 @@ app.use(index);
 
 const server = http.createServer(app);
 
-const io = socketIo(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
-let interval;
+
+const NEW_MESSAGE_EVENT = "newMessage";
 
 io.on("connection", (socket) => {
-  console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  console.log(`Client ${socket.id} connected`);
+
+  // Join a room
+  const { roomId } = socket.handshake.query;
+  socket.join(roomId);
+
+ // Listen for new messages
+  socket.on(NEW_MESSAGE_EVENT, (data) => {
+    console.log(`message dans ${roomId} :  ${data} `);
+    io.in(roomId).emit(NEW_MESSAGE_EVENT, data);
+  });
+
   socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    clearInterval(interval);
+    console.log(`Client ${socket.id} diconnected`);
+    socket.leave(roomId);
   });
 });
 
